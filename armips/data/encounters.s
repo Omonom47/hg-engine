ef4f641609f9dbc1131be315d94aa88843f382b9
Omonom47 <maxih@hotmail.dk>
2025-02-08 18:03:34 +0100

    replace COrsola with Qwilfish in cherrygrove, Make Corsola more likely to appear in Olivine

--- armips/data/encounters.s
+++ armips/data/encounters.s
@@ -299,13 +299,13 @@ encounter SPECIES_KRABBY, 10, 10
 // good rod encounters
 encounter SPECIES_MAGIKARP, 20, 20
 encounter SPECIES_KRABBY, 20, 20
-encounter SPECIES_CORSOLA, 20, 20
-encounter SPECIES_CORSOLA, 20, 20
-encounter SPECIES_CORSOLA, 20, 20s
+encounter SPECIES_QWILFISH, 20, 20
+encounter SPECIES_QWILFISH, 20, 20
+encounter SPECIES_QWILFISH, 20, 20
 
 // super rod encounters
 encounter SPECIES_KRABBY, 40, 40
-encounter SPECIES_CORSOLA, 40, 40
+encounter SPECIES_QWILFISH, 40, 40
 encounter SPECIES_KINGLER, 40, 40
 encounter SPECIES_KINGLER, 40, 40
 encounter SPECIES_KINGLER, 40, 40
@@ -917,22 +917,22 @@ encounter SPECIES_NONE, 0, 0
 // old rod encounters
 encounter SPECIES_MAGIKARP, 10, 10
 encounter SPECIES_MAGIKARP, 10, 10
-encounter SPECIES_MAGIKARP, 10, 10
+encounter SPECIES_TENTACOOL, 10, 10
 encounter SPECIES_TENTACOOL, 10, 10
 encounter SPECIES_TENTACOOL, 10, 10
 
 // good rod encounters
 encounter SPECIES_MAGIKARP, 20, 20
 encounter SPECIES_TENTACOOL, 20, 20
-encounter SPECIES_TENTACOOL, 20, 20
-encounter SPECIES_TENTACOOL, 20, 20
+encounter SPECIES_QWILFISH, 20, 20
+encounter SPECIES_QWILFISH, 20, 20
 encounter SPECIES_QWILFISH, 20, 20
 
 // super rod encounters
 encounter SPECIES_TENTACOOL, 40, 40
+encounter SPECIES_QWILFISH, 40, 40
 encounter SPECIES_TENTACOOL, 40, 40
 encounter SPECIES_MAGIKARP, 40, 40
-encounter SPECIES_QWILFISH, 40, 40
 encounter SPECIES_MAGIKARP, 40, 40
 
 // swarm grass
@@ -4250,10 +4250,10 @@ encounter SPECIES_KRABBY, 10, 10
 encounter SPECIES_KRABBY, 10, 10
 
 // good rod encounters
-encounter SPECIES_MAGIKARP, 20, 20
-encounter SPECIES_KRABBY, 20, 20
 encounter SPECIES_KRABBY, 20, 20
 encounter SPECIES_CORSOLA, 20, 20
+encounter SPECIES_MAGIKARP, 20, 20
+encounter SPECIES_KRABBY, 20, 20
 encounter SPECIES_KRABBY, 20, 20
 
 // super rod encounters
@@ -4261,7 +4261,7 @@ encounter SPECIES_KRABBY, 40, 40
 encounter SPECIES_CORSOLA, 40, 40
 encounter SPECIES_KRABBY, 40, 40
 encounter SPECIES_KINGLER, 40, 40
-encounter SPECIES_KRABBY, 40, 40
+encounter SPECIES_KINGLER, 40, 40
 
 // swarm grass
 pokemon SPECIES_NONE
